1c67f4573a2da6e6ea611fe2ce0314958708efb9
0xMordecai <sir.lahrach.mohamed@gmail.com>
2026-02-05 22:56:57 +0100

    [update terraform-apply bash script]

--- Terraform/terraform-apply.sh
+++ Terraform/terraform-apply.sh
@@ -1,11 +1,11 @@
 # a bash script to automate terraform deployment
 
 set -e
-
+# Prepare your working directory for other commands
 terraform init
-
+# Check whether the configuration is valid
 terraform validate
-
+# Show changes required by the current configuration
 terraform plan
-
+# Create or update infrastructure
 terraform apply
