f3fc5154ef9d35ab1e3cbbd758580ee6d683cf68
0xMordecai <sir.lahrach.mohamed@gmail.com>
2026-02-05 23:29:30 +0100

    [update install-argocd bash script]

--- Manifest/argoCD/install-argocd.sh
+++ Manifest/argoCD/install-argocd.sh
@@ -1,1 +1,4 @@
 #!/usr/bin/env bash
+
+kubectl create namespace argocd
+kubectl apply -n argocd --server-side --force-conflicts -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
