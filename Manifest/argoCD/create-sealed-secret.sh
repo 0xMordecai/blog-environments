60efeba6e056a6b8b358e5b5c48d10fc07753ebb
0xMordecai <sir.lahrach.mohamed@gmail.com>
2026-02-13 23:06:08 +0100

    [update ]

--- Manifest/argoCD/create-sealed-secret.sh
+++ Manifest/argoCD/create-sealed-secret.sh
@@ -5,5 +5,5 @@ set -e
 kubectl create secret generic mongodb-creds \
     --dry-run=client -o yaml --namespace=blog-app \
     --from-literal=MONGO_INITDB_ROOT_USERNAME=root \
-    --from-literal=MONGO_INITDB_ROOT_PASSWORD=<your_pwd> \
+    --from-literal=MONGO_INITDB_ROOT_PASSWORD=root \
     | kubeseal -o yaml > Manifest/application/mongodb-creds-sealed.yaml
