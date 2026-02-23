92b2dfaf7d49b94b2c02df872dbb583f947b4bba
0xMordecai <sir.lahrach.mohamed@gmail.com>
2026-02-23 23:25:01 +0000

    grafana-password.sh[update]

--- Manifest/Monitoring/Prometheus/scripts/grafana-password.sh
+++ Manifest/Monitoring/Prometheus/scripts/grafana-password.sh
@@ -1,2 +1,6 @@
+#!/usr/bin/env bash
+
+set -e
+
 kubectl get secret --namespace monitoring -l app.kubernetes.io/component=admin-secret -o jsonpath="{.items[0].data.admin-password}" | base64 --decode ; echo
 
